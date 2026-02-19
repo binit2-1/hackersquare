9d19016003269899b5b91d1eca9a8d7fbf00cc57
binit <binitgupta.1711@gmail.com>
2026-02-20 00:59:45 +0530

    feat: add UI components including Skeleton, Slider, Sonner, Spinner, Switch, Table, Tabs, Textarea, Toggle Group, Toggle, Tooltip, and custom hook for mobile detection

--- apps/web/src/app/(root)/(landing)/page.tsx
+++ apps/web/src/app/(root)/(landing)/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client";
+
+import { SearchBar } from "@/components/search-bar";
 
 const page = () => {
   return (
-    <div className='w-screen h-screen flex justify-center items-center bg-background text-foreground'>hi</div>
-  )
-}
+    <div className="w-screen h-screen flex justify-center items-center bg-background text-foreground">
+      <SearchBar
+        placeholders={[
+          "Search for anything...",
+          "Try 'React components'",
+          "Ask me anything...",
+        ]}
+        interval={2500}
+        onSubmit={(value: any) => console.log(value)}
+      />
+    </div>
+  );
+};
 
-export default page
+export default page;
