8477a7328bd617a1cbeb9be597ecbbc448c9c59e
binit <binitgupta.1711@gmail.com>
2026-02-21 12:21:35 +0530

    minor fix

--- apps/web/src/app/(root)/(routes)/bookmarks/page.tsx
+++ apps/web/src/app/(root)/(routes)/bookmarks/page.tsx
@@ -4,7 +4,7 @@ const BookmarksPage = () => {
   return (
     <div className="min-h-screen bg-background">
       <div className="max-w-196.5 mx-auto px-4 py-8">
-        {/* Bookmarks content will go here */}
+        Page
       </div>
     </div>
   )
