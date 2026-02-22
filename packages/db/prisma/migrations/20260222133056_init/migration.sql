e4ad08f0b26a919ff8cdcd5f02b8205b313d2f6d
binit <binitgupta.1711@gmail.com>
2026-02-22 19:01:50 +0530

    feat(migration): add initial migration for hackathons table and migration lock

---
+++ packages/db/prisma/migrations/20260222133056_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "hackathons" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "host" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "prize" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "applyUrl" TEXT NOT NULL,
+    "tags" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "hackathons_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "hackathons_id_key" ON "hackathons"("id");
