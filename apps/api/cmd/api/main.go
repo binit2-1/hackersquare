b7fefe42e15fadcc96c0a8f335b3f72ea20462c4
binit <binitgupta.1711@gmail.com>
2026-03-01 12:11:56 +0530

    refactor: using net/http for server

--- apps/api/cmd/api/main.go
+++ apps/api/cmd/api/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+
+func main(){
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("WARN: No .env file found. Relying on system environment variables.")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	}
+
+	mux := http.NewServeMux()
+
+
+	fmt.Printf("Starting server on port %s\n", port)
+
+	if err := http.ListenAndServe(port, mux); err != nil {
+		log.Fatalf("FATAL: Server crashed: %v", err)
+	} 
+	
+
+}
