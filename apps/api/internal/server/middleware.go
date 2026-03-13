580e73c405c6df8ae10cb24cc1858efcc640420d
binit <binitgupta.1711@gmail.com>
2026-03-14 00:54:21 +0530

    feat: update GitHub redirect URIs to use environment variables for better configuration

--- apps/api/internal/server/middleware.go
+++ apps/api/internal/server/middleware.go
@@ -33,6 +33,7 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("NEXT_APP_BASE_URL"))
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
